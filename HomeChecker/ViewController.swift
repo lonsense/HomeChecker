1e55bcd5c3ad69fba3603c73f445da06a67e4a1e
Michael Garden <michael.garden.nz@gmail.com>
2018-05-30 09:43:03 +1200

    Initial Commit

---
+++ HomeChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeChecker
+//
+//  Created by Michael Garden on 30/05/18.
+//  Copyright © 2018 Michael Garden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
